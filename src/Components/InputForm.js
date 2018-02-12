311eb09e4caae96208ee77d98c56c51e4fae93f9
Mahfouzer <mahfouzs96@gmail.com>
2018-02-12 07:03:57 +0200

    finished HTML semantics, added some styling with flex

--- src/Components/InputForm.js
+++ src/Components/InputForm.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
 import './InputForm.css';
+import logo from'./logo.svg';
 
 class InputForm extends Component {
 
 
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {value: 's'};
 
   }
 
+  handleSubmit(event){
+    event.preventDefault();
+    
+  }
+
 handelit(){
-	alert("wooohoo")
+	if (this.state.value === ''){
+		alert("Nope")
+	}
 }
 
   render() {
     return (
-    	<form action="">
+    	<div className="form-container">
+    	<img src={logo} alt="logo"/>
+    	<form onSubmit={this.handleSubmit.bind(this)}>
     	<label htmlFor="Email-Address"> Email Address</label>
-    	<input id="Email-Address" type="email" placeholder="Email" onBlur={this.handelit.bind(this)} />
+    	<input id="Email-Address" type="email" placeholder="Email" onBlur={this.handelit.bind(this)} required/>
+    	<label htmlFor="Password"> Password</label>
+    	<input id="Password" type="password" placeholder="password" onBlur={this.handelit.bind(this)} minLength="8" required/>
+    	<input type="submit" value="Submit" />
     	</form>
+    	</div>
     );
   }
 }
