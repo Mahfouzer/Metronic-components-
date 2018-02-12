0e79ba995984c7d55290cd671cea70f843e9456d
Mahfouzer <mahfouzs96@gmail.com>
2018-02-12 07:47:27 +0200

    increased usability by using state to track changes

--- src/Components/InputForm.js
+++ src/Components/InputForm.js
@@ -7,7 +7,8 @@ class InputForm extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {Emailvalue:"",
+Passwordvalue:""};
 
   }
 
@@ -16,6 +17,14 @@ class InputForm extends Component {
 
     //write the fetch statment and validation code before submiting 
   }
+
+  handleEmailChange(event){
+  	this.setState({Emailvalue:event.target.value})
+  }
+handlePasswordChange(event){
+
+	this.setState({Passwordvalue:event.target.value})
+}
 
 handelEmail(){
 	//further email validation beyond browser validation
@@ -27,17 +36,16 @@ handelPassword(){
 
   render() {
     return (
-
     	<div className="form-container">
 	    	<img src={logo} alt="logo"/>
 	    	{/*The form that accepts the user input*/}
 	    	<form onSubmit={this.handleSubmit.bind(this)}>
 	   			 {/*The email input validated by HTML semantics and browser validation*/}
 	    		<label htmlFor="Email-Address"> Email Address</label>
-	    		<input id="Email-Address" type="email" placeholder="Email" onBlur={this.handelEmail.bind(this)} required/>
+	    		<input id="Email-Address" type="email" placeholder="Email" onBlur={this.handelEmail.bind(this)} value={this.state.Emailvalue} onChange={this.handleEmailChange.bind(this)} required/>
 				 {/*The Password input validated by HTML semantics and browser validation*/}
 	    		<label htmlFor="Password"> Password</label>
-	    		<input id="Password" type="password" placeholder="password" onBlur={this.handelPassword.bind(this)} minLength="8" required/>
+	    		<input id="Password" type="password" placeholder="password" onBlur={this.handelPassword.bind(this)} minLength="8" value={this.state.Passwordvalue} onChange={this.handlePasswordChange.bind(this)} required/>
 	    		{/*the submit button*/}
 	    		<input type="submit" value="Submit" />
 	    	</form>
