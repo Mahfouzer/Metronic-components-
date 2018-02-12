4fb44217674e9ee8f879df60fc98d21ff4cacb04
Mahfouzer <mahfouzs96@gmail.com>
2018-02-12 07:26:43 +0200

    add comments for the documintation in the InputForm comp.

--- src/Components/InputForm.js
+++ src/Components/InputForm.js
@@ -7,13 +7,14 @@ class InputForm extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {value: 's'};
+    this.state = {value: ''};
 
   }
 
   handleSubmit(event){
     event.preventDefault();
-    
+
+    //write the fetch statment and validation code before submiting 
   }
 
 handelit(){
@@ -24,15 +25,20 @@ handelit(){
 
   render() {
     return (
+		{/*The container for styling purposes*/}
     	<div className="form-container">
-    	<img src={logo} alt="logo"/>
-    	<form onSubmit={this.handleSubmit.bind(this)}>
-    	<label htmlFor="Email-Address"> Email Address</label>
-    	<input id="Email-Address" type="email" placeholder="Email" onBlur={this.handelit.bind(this)} required/>
-    	<label htmlFor="Password"> Password</label>
-    	<input id="Password" type="password" placeholder="password" onBlur={this.handelit.bind(this)} minLength="8" required/>
-    	<input type="submit" value="Submit" />
-    	</form>
+	    	<img src={logo} alt="logo"/>
+	    	{/*The form that accepts the user input*/}
+	    	<form onSubmit={this.handleSubmit.bind(this)}>
+	   			 {/*The email input validated by HTML semantics and browser validation*/}
+	    		<label htmlFor="Email-Address"> Email Address</label>
+	    		<input id="Email-Address" type="email" placeholder="Email" onBlur={this.handelit.bind(this)} required/>
+				 {/*The Password input validated by HTML semantics and browser validation*/}
+	    		<label htmlFor="Password"> Password</label>
+	    		<input id="Password" type="password" placeholder="password" onBlur={this.handelit.bind(this)} minLength="8" required/>
+	    		{/*the submit button*/}
+	    		<input type="submit" value="Submit" />
+	    	</form>
     	</div>
     );
   }
