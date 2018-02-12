6966f0dd0c55e49b06cbd42f96f98a2e09185358
Mahfouzer <mahfouzs96@gmail.com>
2018-02-12 07:34:48 +0200

    fixed an issue with a comment added validation handelers

--- src/Components/InputForm.js
+++ src/Components/InputForm.js
@@ -17,25 +17,27 @@ class InputForm extends Component {
     //write the fetch statment and validation code before submiting 
   }
 
-handelit(){
-	if (this.state.value === ''){
-		alert("Nope")
-	}
+handelEmail(){
+	//further email validation beyond browser validation
+}
+
+handelPassword(){
+		//further Password validation beyond browser validation
 }
 
   render() {
     return (
-		{/*The container for styling purposes*/}
+
     	<div className="form-container">
 	    	<img src={logo} alt="logo"/>
 	    	{/*The form that accepts the user input*/}
 	    	<form onSubmit={this.handleSubmit.bind(this)}>
 	   			 {/*The email input validated by HTML semantics and browser validation*/}
 	    		<label htmlFor="Email-Address"> Email Address</label>
-	    		<input id="Email-Address" type="email" placeholder="Email" onBlur={this.handelit.bind(this)} required/>
+	    		<input id="Email-Address" type="email" placeholder="Email" onBlur={this.handelEmail.bind(this)} required/>
 				 {/*The Password input validated by HTML semantics and browser validation*/}
 	    		<label htmlFor="Password"> Password</label>
-	    		<input id="Password" type="password" placeholder="password" onBlur={this.handelit.bind(this)} minLength="8" required/>
+	    		<input id="Password" type="password" placeholder="password" onBlur={this.handelPassword.bind(this)} minLength="8" required/>
 	    		{/*the submit button*/}
 	    		<input type="submit" value="Submit" />
 	    	</form>
